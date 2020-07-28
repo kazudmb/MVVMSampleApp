b7aacd7b6fc60cb3feeeecb80f4e61bb77f0624b
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-28 14:13:19 +0900

    Initial commit
    https://youtu.be/RkRX446RSPk

---
+++ app/src/main/java/com/dmb/mvvmsampleapp/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.dmb.mvvmsampleapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dmb.mvvmsampleapp.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
